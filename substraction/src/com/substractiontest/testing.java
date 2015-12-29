7da309547e645e83ce8bfa13823fd82ae7dc3b42
kush8007 <kushpatel3940@gmail.com>
2015-12-28 18:46:00 -0600

    J unit 1
    J unit 1

---
+++ substraction/src/com/substractiontest/testing.java
@@ -0,0 +1,56 @@
+package com.substractiontest;
+
+public class testing {
+
+	public static int addnumbeWithReturnType() {
+		int a = 2;
+		int b = 30; 
+		
+
+		int c = a+b;
+		
+		  System.out.println("resul from additional with return type;"+c);
+		  
+		  return c;
+		  
+}
+	//with a return type of int
+	public static int addnumbersWithReturnTypeAndAcceptanceParameters(int a,int b){
+		
+		int c = a+b;
+		
+		System.out.println("result  from addition with return type;"+c);
+		
+		return c;
+	}
+	      public static void main(String args[]){
+			
+		 addnumbersWithReturnTypeAndAcceptanceParameters(10.15);
+		 
+		
+			
+			
+		
+		
+		
+		
+	}private static int addnumbersWithReturnTypeAndAcceptanceParameters(double d) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+	{
+		
+	
+		
+			
+		
+
+		
+		}
+		
+		
+
+
+	
+}
---
+++ substraction/test/com/substractiontest/SubstractionTest.java
@@ -0,0 +1,6 @@
+package com.substractiontest;
+
+public class SubstractionTest {
+	
+
+}
